4905e9096a99ea903015295b12653356e5c5df7c
Dheeraj Kaushik <dheerajkaushik428@gmail.com>
2026-03-23 15:34:30 +0530

    Fixed OTP Issue

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,11 +1,13 @@
 const { Resend } = require('resend');
+require('dotenv').config();
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOTPEmail = async (email, otp) => {
     try {
-        await resend.emails.send({
+        console.log("Sending to:", email);
+        const result = await resend.emails.send({
             from: 'onboarding@resend.dev',
-            to: email,
+            to: [email],
             subject: 'Verify your CampusLink account',
             html: `
                 <h2>CampusLink Verification</h2>
@@ -16,7 +18,10 @@ const sendOTPEmail = async (email, otp) => {
         });
 
         console.log("✅ Email sent via Resend");
+        console.log("✅ Email sent:", result);
     } catch (err) {
         console.log("❌ Email error:", err);
     }
 };
+
+module.exports = { sendOTPEmail };
