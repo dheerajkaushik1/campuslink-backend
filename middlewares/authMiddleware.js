4b2ae52dd9adc5931b85530065de9261987b80b6
Dheeraj Kaushik <dheerajkaushik428@gmail.com>
2026-08-21 19:38:31 +0530

    Fixed Login Expire Issue

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,20 +1,36 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
-    try{
-        const authHeader = req.headers.authorization
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({
+                message: "No token, authorization denied!",
+            });
+        }
 
         const token = authHeader.split(" ")[1];
-        if(!token){
-            return res.status(401).json({message: "No token, authorization denied!"});
+
+        if (!token) {
+            return res.status(401).json({
+                message: "No token, authorization denied!",
+            });
         }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
         req.user = decoded;
 
         next();
     } catch (err) {
-        res.status(500).json({message: "Server Error", eroor: err.message});
+        return res.status(401).json({
+            message: "Token expired or invalid!",
+        });
     }
 };
 
-module.exports = {protect};
+module.exports = { protect };
